653cd7c5d60df6acb77ac13923c48100bcbc615e
Jozimar Back <jozimar5@gmail.com>
2020-08-09 19:58:29 -0300

    Criação de obtenção de dados por id

--- app/main/home/home_controller.py
+++ app/main/home/home_controller.py
@@ -7,3 +7,16 @@ class HomeController(Resource):
     @api.response(200, "Busca realizada com sucesso")
     def get(self):
         return {"mensagem": "Home"}, 200
+    def post(self):
+        pass
+
+@api.route('/<id>')
+class HomeIdController(Resource):
+    @api.response(200, "Busca realizada com sucesso")
+    def get(self, id):
+        return {"mensagem": "Home"}, 200
+
+    @api.response(200, "Busca realizada com sucesso")
+    def put(self, id):
+        return {"mensagem": "Home"}, 200
+
